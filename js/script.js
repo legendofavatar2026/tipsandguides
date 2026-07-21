87f529d8ce02872f3f9f181183cc853fb6ee5edb
legendofavatar2026 <lifendarkness@gmail.com>
2026-07-21 11:32:36 +0800

    popup revision

--- js/script.js
+++ js/script.js
@@ -17,16 +17,24 @@ search.addEventListener("keyup",function(){
 });
 
 const popup = document.getElementById("updatePopup");
-
 const closeBtn = document.querySelector(".close-popup");
+
+// Change this whenever you update the popup
+const popupVersion = "LOA-1.23.0";
+
+// Show popup only if user hasn't seen this version
+if (localStorage.getItem("popupVersion") !== popupVersion) {
+    popup.style.display = "flex";
+}
 
 closeBtn.onclick = () => {
     popup.style.display = "none";
+    localStorage.setItem("popupVersion", popupVersion);
 };
 
 window.onclick = (e) => {
-    if(e.target === popup){
+    if (e.target === popup) {
         popup.style.display = "none";
+        localStorage.setItem("popupVersion", popupVersion);
     }
 };
-
