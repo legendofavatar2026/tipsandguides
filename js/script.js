b461305acc9026704480f479f8217f61a664a327
legendofavatar2026 <lifendarkness@gmail.com>
2026-08-03 20:52:44 +0800

    august roadmap update

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ const popup = document.getElementById("updatePopup");
 const closeBtn = document.querySelector(".close-popup");
 
 // Change this whenever you update the popup
-const popupVersion = "LOA-1.24.1";
+const popupVersion = "LOA-AUGUST-ROADMAP";
 
 // Show popup only if this version hasn't been seen
 if (localStorage.getItem("popupVersion") !== popupVersion) {
