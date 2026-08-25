c6f1a66b9f288c4e9eb5bdb488332d1a48a3fb05
legendofavatar2026 <lifendarkness@gmail.com>
2026-08-25 20:29:03 +0800

    upcoming guild raid update

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ const popup = document.getElementById("updatePopup");
 const closeBtn = document.querySelector(".close-popup");
 
 // Change this whenever you update the popup
-const popupVersion = "LOA-1.25.0-LIVE";
+const popupVersion = "LOA-1.26.0-PREVIEW";
 
 // Show popup only if this version hasn't been seen
 if (localStorage.getItem("popupVersion") !== popupVersion) {
