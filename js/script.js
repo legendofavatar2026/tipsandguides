a40908b7b811f94a1aebe618a2f95c0d33fd9429
legendofavatar2026 <lifendarkness@gmail.com>
2026-08-31 19:05:57 +0800

    september update

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ const popup = document.getElementById("updatePopup");
 const closeBtn = document.querySelector(".close-popup");
 
 // Change this whenever you update the popup
-const popupVersion = "LOA-1.26.0-PREVIEW";
+const popupVersion = "LOA-September_UPDTE";
 
 // Show popup only if this version hasn't been seen
 if (localStorage.getItem("popupVersion") !== popupVersion) {
