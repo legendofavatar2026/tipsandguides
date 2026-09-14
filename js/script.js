b2d58d8b4cd4d1a00dd5538eea21fd88a4dee751
legendofavatar2026 <lifendarkness@gmail.com>
2026-09-14 21:23:45 +0800

    preview for version 1.27

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ const popup = document.getElementById("updatePopup");
 const closeBtn = document.querySelector(".close-popup");
 
 // Change this whenever you update the popup
-const popupVersion = "LOA-Shizuka-update-LIVE";
+const popupVersion = "LOA-V1.27.0_preview";
 
 // Show popup only if this version hasn't been seen
 if (localStorage.getItem("popupVersion") !== popupVersion) {
